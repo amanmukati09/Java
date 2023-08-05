4f762f55c878bf22e216ca8155e25354be5088f9
= <=>
2023-08-05 13:14:52 +0530

    Get timezones

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.time.ZoneId;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println(ZoneId.systemDefault());
+        ZoneId.getAvailableZoneIds().stream().filter(s -> s.startsWith("Asia")).sorted().map(ZoneId::of).forEach(z -> System.out.println(z.getId() + " " + z.getRules()));
+
     }
 }
